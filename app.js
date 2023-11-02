9f82901389476ab84a633d522e7fcfe3a62b711f
Carl Domingue <carl.domingue@gcpsk12.org>
2023-11-02 12:47:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Inspired by https://huggingface.co/spaces/huggingfacejs/text-generation-Mistral-7B-Instruct
+
+import { HfInference } from '@huggingface/inference'
+
+
+async function* textStreamRes(hf, controller, input) {
+  let tokens = [];
+  for await (const output of hf.textGenerationStream(
+    {
+      model: "mistralai/Mistral-7B-Instruct-v0.1",
+      inputs: "<s>[INST]Write an essay about Sartre[/INST]",
+      parameters: { max_new_tokens: 1000 },
+    },
+    {
+      use_cache: false,
+      signal: controller.signal,
+    }
+  )) {
+    tokens.push(output);
+    yield tokens;
+  }
+}
+
+let controller;
+async function run() {
+  controller = new AbortController();
+  const message = `<s>[INST]{:}[/INST]`;
+  const textInput = 'Write an essay about Sartre';
+  const input = message.replace("{:}", textInput);
+  const token = '';
+  const hf = new HfInference(token);
+
+  try {
+    for await (const tokens of textStreamRes(hf, controller, input)) {
+      const lastToken = tokens[tokens.length - 1];
+      process.stdout.write(lastToken.token.text);
+    }
+  } catch (e) {
+    console.log("aborted");
+  }
+}
+
+
+run();
+
